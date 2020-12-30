4bd6c3f8f9f31667f50c6f4911212b20f6a79ebe
Matan Lurey <matan@lurey.org>
2020-12-29 17:47:10 -0800

    Basic ass grid.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,17 @@
+import { Layout } from 'antd';
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import './App.css';
-import logo from './logo.svg';
+import './App.scss';
+import { Board } from './Board';
 
 function app(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Layout.Header></Layout.Header>
+      <Layout.Content>
+        <Board />
+      </Layout.Content>
+    </Layout>
   );
 }
 
